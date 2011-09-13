1c4f15f0265adaf8577abd7a76cd429bf8623f09
James Pickup <james@epickup.co.uk>
2011-09-13 02:40:56 +0100

    Draft design with Less framework as base

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,11 +1,11 @@
 (function() {
-  var arc, arcs, color, data, donut, h, paths, r, tweenDonut, tweenPie, vis, w;
+  var arc, arcs, b_h, b_w, barHeight, bars, color, data, donut, p_h, p_w, paths, r, tweenDonut, tweenPie, vis, x, y;
   tweenPie = function(b) {
     var i;
     b.innerRadius = 0;
     i = d3.interpolate({
       startAngle: 0,
-      endAngle: 0
+      endAngle: 100
     }, b);
     return function(t) {
       return arc(i(t));
@@ -13,7 +13,7 @@
   };
   tweenDonut = function(b) {
     var i;
-    b.innerRadius = r * .6;
+    b.innerRadius = r * .7;
     i = d3.interpolate({
       innerRadius: 0
     }, b);
@@ -21,20 +21,79 @@
       return arc(i(t));
     };
   };
-  w = 600;
-  h = 600;
-  r = Math.min(w, h) / 2;
-  data = d3.range(5).map(Math.random);
+  p_w = 344;
+  p_h = 344;
+  r = p_w / 2;
+  data = [
+    {
+      x: "TLodge",
+      y: 40000
+    }, {
+      x: "TRodden",
+      y: 60000
+    }, {
+      x: "RSpencer",
+      y: 25000
+    }, {
+      x: "KGlover",
+      y: 55000
+    }, {
+      x: "Mort",
+      y: 65000
+    }, {
+      x: "RSpencer",
+      y: 25000
+    }, {
+      x: "KGlover",
+      y: 55000
+    }, {
+      x: "Mort",
+      y: 65000
+    }, {
+      x: "RSpencer",
+      y: 25000
+    }, {
+      x: "KGlover",
+      y: 55000
+    }, {
+      x: "Mort",
+      y: 65000
+    }, {
+      x: "RSpencer",
+      y: 25000
+    }, {
+      x: "KGlover",
+      y: 55000
+    }, {
+      x: "Mort",
+      y: 65000
+    }
+  ];
   color = d3.scale.category20();
   arc = d3.svg.arc().outerRadius(r);
-  donut = d3.layout.pie();
-  vis = d3.select("body").append("svg:svg").data([data.sort(d3.descending)]).attr("width", w).attr("height", h);
+  donut = d3.layout.pie().value(function(d) {
+    return d.y;
+  });
+  vis = d3.select("#pie_chart").append("svg:svg").data([data]).attr("width", p_w).attr("height", p_h);
   arcs = vis.selectAll("g.arc").data(donut).enter().append("svg:g").attr("class", "arc").attr("transform", "translate(" + r + "," + r + ")");
   paths = arcs.append("svg:path").attr("fill", function(d, i) {
     return color(i);
   });
-  paths.transition().ease("bounce").duration(2000).attrTween("d", tweenPie);
-  paths.transition().ease("elastic").delay(function(d, i) {
-    return 2000 + i * 50;
-  }).duration(750).attrTween("d", tweenDonut);
+  paths.transition().ease("elastic").duration(750).attrTween("d", tweenDonut);
+  barHeight = 40;
+  b_h = (barHeight + 10) * data.length;
+  b_w = 528;
+  console.log(b_h, b_w, data.length);
+  x = d3.scale.linear().domain([
+    0, d3.max(data, function(datum) {
+      return datum.y;
+    })
+  ]).rangeRound([0, b_w]);
+  y = d3.scale.linear().domain([0, data.length]).range([0, b_h]);
+  bars = d3.select("#bar_graph").append("svg:svg").attr("width", b_w).attr("height", b_h);
+  bars.selectAll("rect").data(data).enter().append("svg:rect").attr("y", function(datum, index) {
+    return y(index);
+  }).attr("height", barHeight).attr("width", function(datum) {
+    return x(datum.y);
+  }).attr("fill", "#2D578B");
 }).call(this);
--- public_src/dashboard.coffee
+++ public_src/dashboard.coffee
@@ -1,45 +1,81 @@
 tweenPie = (b) ->
   b.innerRadius = 0
-  i = d3.interpolate({startAngle: 0, endAngle: 0}, b)
+  i = d3.interpolate({startAngle: 0, endAngle: 100}, b)
   return (t) ->
     return arc(i(t))
  
 tweenDonut = (b) ->
-  b.innerRadius = r * .6
+  b.innerRadius = r * .7
   i = d3.interpolate({innerRadius: 0}, b)
   return (t) ->
     return arc(i(t))
 
-w = 600
-h = 600
-r = Math.min(w, h) / 2
+p_w = 344
+p_h = 344
+r = p_w / 2
 
-data    = d3.range(5).map(Math.random)
+data    = [
+            {x: "TLodge", y: 40000 },
+            {x: "TRodden", y: 60000 },
+            {x: "RSpencer", y: 25000 },
+            {x: "KGlover", y: 55000 },
+            {x: "Mort", y: 65000 },
+               {x: "RSpencer", y: 25000 },
+            {x: "KGlover", y: 55000 },
+            {x: "Mort", y: 65000 },
+            {x: "RSpencer", y: 25000 },
+            {x: "KGlover", y: 55000 },
+            {x: "Mort", y: 65000 },
+            {x: "RSpencer", y: 25000 },
+            {x: "KGlover", y: 55000 },
+            {x: "Mort", y: 65000 }
+          ]
+
 color   = d3.scale.category20()
 arc     = d3.svg.arc().outerRadius(r)
 donut   = d3.layout.pie()
+          .value( (d) -> return d.y )
 
-vis     = d3.select("body")
+vis     = d3.select("#pie_chart")
           .append("svg:svg")
-          .data([data.sort(d3.descending)])
-          .attr("width", w)
-          .attr("height", h)
-
+          .data([data])
+          .attr("width", p_w)
+          .attr("height", p_h)
 arcs    = vis.selectAll("g.arc")
           .data(donut)
           .enter().append("svg:g")
           .attr("class", "arc")
-          .attr("transform", "translate(" + r + "," + r + ")")
+          .attr("transform", "translate(" + r +  "," + r + ")")
  
 paths   = arcs.append("svg:path")
           .attr("fill", (d, i) -> color(i) )
-paths.transition()
-    .ease("bounce")
-    .duration(2000)
-    .attrTween("d", tweenPie)
- 
+    #      .attr("d", arc)
 paths.transition()
     .ease("elastic")
-    .delay((d, i) -> (2000 + i * 50))
+    #.delay((d, i) -> (2000 + i * 50))
     .duration(750)
     .attrTween("d", tweenDonut)
+
+barHeight = 40
+b_h = (barHeight + 10) * data.length
+b_w = 528
+
+console.log b_h, b_w, data.length
+
+x = d3.scale.linear().domain([0, d3.max(data, (datum) -> return datum.y)])
+    .rangeRound([0,b_w])
+y = d3.scale.linear().domain([0, data.length]).range([0, b_h])
+
+bars = d3.select("#bar_graph")
+        .append("svg:svg")
+        .attr("width", b_w)
+        .attr("height", b_h)
+        
+bars.selectAll("rect")
+      .data(data)
+      .enter()
+      .append("svg:rect")
+      .attr("y", (datum, index) -> return y(index) )
+      .attr("height", barHeight)
+      .attr("width", (datum) -> return x(datum.y) )
+      .attr("fill", "#2D578B")
